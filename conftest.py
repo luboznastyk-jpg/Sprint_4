030b90c24acaf570dc673178b913fb5af6dbc284
luboznastyk-jpg <luboznastyk@gmail.com>
2025-11-26 18:27:56 +0300

    исправить обращение к фикстуре в тестовых методах, добавить зависимость фикстур

--- conftest.py
+++ conftest.py
@@ -4,12 +4,12 @@ from main import BooksCollector
 
 from data import libr
 
-@pytest.fixture 
-def collector(scope = 'session'):
+@pytest.fixture ()
+def collector():
     collector = BooksCollector()
     return collector
 
 @pytest.fixture
-def add_book():
+def add_book(collector):
     collector.books_genre = libr
     return collector
--- tests_books_collector.py
+++ tests_books_collector.py
@@ -14,8 +14,8 @@ class TestBooksCollector:
         assert len(list(collector.books_genre.keys()))  == len(book_names)
 
 
-    def test_set_book_genre_book_in_books_genre(self, collector, add_book):
-        
+    def test_set_book_genre_book_in_books_genre(self, add_book):
+
         add_book.set_book_genre(my_books[0], genre[0])
         assert add_book.books_genre[my_books[0]] == genre[0]
 
@@ -32,23 +32,22 @@ class TestBooksCollector:
         collector.set_book_genre(book_name, genre)
         assert collector.get_books_with_specific_genre(genre) == [book_name]
 
-    def test_get_books_genre_dict(self, collector, add_book):
+    def test_get_books_genre_dict(self, add_book):
 
-         assert add_book.get_books_genre() == libr
+        assert add_book.get_books_genre() == libr
 
 
-    def test_get_books_for_children(self, collector, add_book):
+    def test_get_books_for_children(self, add_book):
 
         assert "Детская книга" in add_book.get_books_for_children()
         assert "Взрослая книга" not in add_book.get_books_for_children()
 
 
     @pytest.mark.parametrize("book_name", favorites)
-    def test_add_book_in_favorites(self, collector, book_name):
-        
-        collector.add_new_book(book_name)
-        collector.add_book_in_favorites(book_name)
-        assert book_name in collector.favorites
+    def test_add_book_in_favorites(self, book_name, add_book):
+
+        add_book.add_book_in_favorites(book_name)
+        assert book_name in add_book.favorites
 
     def test_delete_book_from_favorites(self, collector):
         
@@ -57,8 +56,7 @@ class TestBooksCollector:
         collector.delete_book_from_favorites('book_name')
         assert 'book_name' not in collector.favorites
 
-
-    def test_get_list_of_favorites_books(self, collector,  add_book):
+    def test_get_list_of_favorites_books(self, add_book):
         for favorite in favorites:
             add_book.add_book_in_favorites(favorite)
         assert add_book.get_list_of_favorites_books() == favorites
